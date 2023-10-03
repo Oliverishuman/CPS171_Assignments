0f421726fb9e2471a0c51900400bdb952a9b4126
Oliverishuman <mcmilleno@yahoo.com>
2023-10-02 23:04:20 -0400

    Updates to MP3 10-02-2023 11:04:20

--- CPS171_MP3/CPS171_MP3/main.cpp
+++ CPS171_MP3/CPS171_MP3/main.cpp
@@ -6,13 +6,38 @@
 //
 
 #include <iostream>
+#include <string>
+#include <iomanip>
+#include <fstream>
 
 using namespace std;
 
+ifstream inFile;
+string str;
+
+//TODO
+//Use array to ready in data and interpret, then use if else/switch statement on that array segment, then erase array each record
+int cellData[3];
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    cout << "Testing!\\\\n";
-    cout << "This is another line \n";
-    cout << "Third line here... \n";
+    
+    inFile.open("dataFileIn.txt");
+    
+    if (inFile.fail())
+        cout << endl << "File not found!" << endl;
+    else{
+        // While no end of file error exists, read in data
+        while(!inFile.eof()){
+            getline(inFile, str);
+            cout << str;
+            
+        
+            
+            cout << endl;
+        }
+        inFile.close();
+    }
+    
+    
     return 0;
 }
