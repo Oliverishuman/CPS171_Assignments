bb23f7e7a7dc856107d854b05db9f54494532272
Oliverishuman <mcmilleno@yahoo.com>
2023-10-04 22:48:06 -0400

    Updating MP2 10-04-23 10:47:50 PM

--- CPS171_MP2/CPS171_MP2/main.cpp
+++ CPS171_MP2/CPS171_MP2/main.cpp
@@ -1,66 +1,82 @@
-//  CPS171_MP2
-//
-//  Created by Oliver McMillen on 9/6/23.
-//
+/*
+  CPS271_MP2 - KERBAA Train report
+
+  This program is to read 4 lines of data from a .txt file in the source folder, the 4 lines of data each represent the name of railroad,
+  the destination, number of ore cars, and the current price of coal per short ton. In return, the program will write the derived data to a .txt file
+  returning the total weight of coal, total value of shipment, and total surcharge of the shipment.
+
+  Created by Oliver McMillen on 9/12/23.
+*/
 
 #include <iostream>
-#include <string>
-#include <iomanip>
 #include <fstream>
+#include <iomanip>
 
 using namespace std;
 
-ifstream inFile;
+ifstream fin;
+ofstream fout;
 string str;
-
 string railroadName;
 string destination;
+const double carWeightCapacity = 74.5;
+const double currentSurcharge = 0.0031;
 int numOreCars;
 double pricePerShortTonCoal;
 double totalShipmentWeight;
 double totalShipmentValue;
 double totalShipmentSurcharge;
-const double carCapacity = 74.5;
-const double currentSurcharge = 0.0031;
 
 
 int main(int argc, const char * argv[]) {
-    inFile.open("dataFileIn.txt");
+    fin.open("dataFileIn.txt");
     
-    if (inFile.fail())
+    // Opens .txt file and reads expected 4 lines of data, if the specified file is not found, echo "File not found!" to the console
+    if (fin.fail())
         cout << endl << "File not found!" << endl;
     else{
-            getline(inFile, str);
+            getline(fin, str);
             railroadName = str;
         
-            getline(inFile, str);
+            getline(fin, str);
             destination = str;
         
-            getline(inFile, str);
+            getline(fin, str);
             numOreCars = stoi(str);
         
-            getline(inFile, str);
+            getline(fin, str);
             pricePerShortTonCoal = stof(str);
         
-        inFile.close();
+        fin.close();
     }
     
-    totalShipmentWeight = (numOreCars * carCapacity);
+    totalShipmentWeight = (numOreCars * carWeightCapacity);
     totalShipmentValue = (totalShipmentWeight * pricePerShortTonCoal);
     totalShipmentSurcharge = (totalShipmentValue * currentSurcharge);
     
-    cout << "KERBAA & M COAL TRAIN REPORT – Oliver McMillen" << endl << endl;
-    
-    cout << "Railroad name: " << railroadName << endl;
-    cout << "Destination: " << destination << endl << endl;
-    
-    cout << "Number of ore cars: " << numOreCars << endl;
-    cout << "Total weight of shipment: " << totalShipmentWeight << " short tons" << endl;
-    cout << "Current Cost per short ton: " << pricePerShortTonCoal << endl;
-    cout << "Total value of shipment: " << totalShipmentValue << endl;  //For some reason the double isn't taking affect
-    cout << "Current surcharge: " << currentSurcharge * 100 << "%" << endl;
+    // Creates .txt file to write data to, if sytem cannot create new file it will echo "File couldn't be opened!" to the console.
+    fout.open("dataFileOut.txt");
 
-    cout << "Total Surcharge(EST): " << totalShipmentSurcharge << endl << endl;
+    if (fout.fail())
+        fout << endl << "File couldn't be opend!" << endl;
+    else{
+        fout << "KERBAA & M COAL TRAIN REPORT – Oliver McMillen" << endl << endl;
 
+        fout << "Railroad name: " << railroadName << endl;
+        fout << "Destination: " << destination << endl << endl;
+
+        fout << "Number of ore cars:" << setfill('.') << setw(12) << "." << setfill(' ') << right << setw(13) << numOreCars << endl;
+        fout << fixed << setprecision(2);
+        fout << "Total weight of coal" << setfill('.') << setw(11) << "." << setfill(' ') << right << setw(16) << totalShipmentWeight << " short tons" << endl;
+        fout << "Current Cost per short ton" << setfill('.') << setw(6) << "$" << setfill(' ') << right << setw(15) << pricePerShortTonCoal << endl;
+        fout << "Total value of shipment" << setfill('.') << setw(9) << "$" << setfill(' ')  << right << setw(15) << totalShipmentValue << endl;  //For some reason the double isn't taking affect
+        fout << "Current surcharge" << setfill('.') << setw(14) << "." << setfill(' ') << right << setw(16) << currentSurcharge * 100 << " %" << endl;
+
+        fout << "Total Surcharge(Est)" << setfill('.') << setw(12) << "$" << setfill(' ') <<  right << setw(15) << totalShipmentSurcharge << endl << endl;
+
+        fout.close();
+        cout << "File written successfully" << endl;
+    }
+    
     return 0;
 }
