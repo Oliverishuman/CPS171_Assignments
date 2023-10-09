57bb40973b1d793766f01a0d6337730bfd8a9dcf
Oliverishuman <mcmilleno@yahoo.com>
2023-10-09 16:41:39 -0400

    Updates to MP3 10-09-23 4:40:45 PM
    - Cleaned up variables and constants.
    
    - Cleaned up the cost variable and functionality when a record is not calculated for minutes charged.

--- CPS171_MP3/CPS171_MP3/main.cpp
+++ CPS171_MP3/CPS171_MP3/main.cpp
@@ -7,10 +7,11 @@
  The program will read each record, declare whether it's a day or night call, how many minutes should be charged based on the
  requirements, and charge the proper rate according to the type of call.
  
- It will output a list to the console containing a header with the column names, resulting with a total monthly bill amount.
+ It will output a list to the console containing a header with the column names and data from the text file, as well as the total
+ cost of minutes charged over 200 minutes and the monthly bill total costOfCall.
  
  Created by Oliver McMillen on 10/2/23.
-*/
+ */
 
 #include <iostream>
 #include <string>
@@ -18,11 +19,10 @@
 #include <fstream>
 
 using namespace std;
-const int dayCallStart = 600;
-const int dayCallEnd = 1759;
-const int nightCallStart = 1800;
-const int nightCallEnd = 559;
-const int monthlyMinuteLimit = 200;
+const int dayCallStart = 600; //Day call starts at 6 AM
+const int dayCallEnd = 1759; //Day call ends at 5:59 PM
+const int monthlyMinuteLimit = 200; //The number of minutes the basic monthly rate covers
+const double basicMonthlyRate = 39.99; //The $ amount of the basic monthly rate of 200 minutes
 
 int main(int argc, const char * argv[]) {
     ifstream inFile;
@@ -33,25 +33,27 @@ int main(int argc, const char * argv[]) {
     int minutesUsedTotal = 0;
     int chargedMinutes = 0;
     double monthlyBillTotal = 0.0;
-    double cost;
+    double over200MinutesTotalCost = 0.0;
+    double costOfCall = 0.00;
     bool isDayCall;
     
     
     inFile.open("dataFileIn.txt");
     
+    //If file is not found, echo error message to console
     if (inFile.fail())
         cout << endl << "File not found!" << endl;
     else{
         
         cout << "Cellphone charges - MP3 by Oliver McMillen" << endl << endl;
         cout << setw(8) << left << "Time" << setw(10) << "Duration" << setw(15) << "Type of call" << setw(23)
-        << "Minutes used so far" << "Cost" << endl;
+        << "Minutes used so far" << "costOfCall" << endl;
         
-        //While file instream is not empty
+        //While file instream is not empty, do this
         while(inFile >> timeCallMade >> durationOfCall >> typeOfCall){
             futureMinutesUsed = durationOfCall + minutesUsedTotal;
             
-            //If the call is made after 6AM and before 5:59 PM then it is a day call
+            //If the call is made at or after dayCallStart && at or before dayCallEnd, then it is a day call
             if (timeCallMade >= dayCallStart && timeCallMade <= dayCallEnd){
                 isDayCall = true;
             }
@@ -59,40 +61,51 @@ int main(int argc, const char * argv[]) {
                 isDayCall = false;
             }
             
-            //If it is a night call, else if it is a day call
+            
             if (isDayCall == false){
-                cout << left << setw(8) << timeCallMade << setw(10) << durationOfCall << setw(15) << typeOfCall << setw(23) << "Night call is free"  << endl;
+                cout << left << setw(8) << timeCallMade << setw(10) << durationOfCall << setw(15) << typeOfCall
+                << setw(23) << "Night call is free"  << endl;
             }
             else{
                 
-                //If current minutes used is less than 200 and future minutes used will be greater than 200
+                //If current minutes used is less than 200 and future minutes used is greater than 200, then calculate charged minutes
                 if (minutesUsedTotal < monthlyMinuteLimit && futureMinutesUsed > monthlyMinuteLimit){
-                    //Calculate charged minutes over 200
                     chargedMinutes = (futureMinutesUsed - monthlyMinuteLimit);
+                    
+                    //If minutes used is greater or equal to 200, then the duration of call equals charged minutes
                 } else if (minutesUsedTotal >= monthlyMinuteLimit){
-                    //If minutes used is greater or equal to 200, duration of call equals charged minutes
                     chargedMinutes = durationOfCall;
+                } else {
+                    chargedMinutes = 0;
                 }
                 
                 //Accumulates total minutes used
                 minutesUsedTotal += durationOfCall;
                 
+                //If type of call is Local Area
                 if (typeOfCall == 'A'){
-                    cost = chargedMinutes * (0.29);
+                    costOfCall = chargedMinutes * (0.29);
+                //If type of call is long distance
                 } else if (typeOfCall == 'L'){
-                    cost = chargedMinutes * (0.69);
-                } else {
-                    cost = 0.00;
+                    costOfCall = chargedMinutes * (0.69);
                 }
                 
                 cout << fixed << left << setw(8) << timeCallMade << setw(10) << durationOfCall << setw(15)
-                << typeOfCall << setw(23) << minutesUsedTotal << setprecision(2) << "$" << cost << endl;
+                << typeOfCall << setw(23) << minutesUsedTotal << setprecision(2) << "$" << costOfCall << endl;
                 
-                monthlyBillTotal += cost;
+                //Accumulates the total costOfCall of charges over 200 minutes
+                over200MinutesTotalCost += costOfCall;
             }
         }
     }
+    monthlyBillTotal = over200MinutesTotalCost + basicMonthlyRate;
     
-    cout << "\nThe monthly bill is $" << fixed << setprecision(2) << monthlyBillTotal << endl;
+    cout << "\nThe total charged over 200 minutes is $" << fixed << setprecision(2) << over200MinutesTotalCost
+    << endl;
+    
+    cout << "The monthly bill total is $" << fixed << setprecision(2) << monthlyBillTotal
+    << " (Includes the basic monthly rate of $" << basicMonthlyRate << ")" << endl << endl;
+    
+    inFile.close();
     return 0;
 }
