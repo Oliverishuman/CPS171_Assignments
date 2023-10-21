55bab3fe48c27ad783c1b6a40789191b5d177273
Oliverishuman <mcmilleno@yahoo.com>
2023-10-20 22:37:02 -0400

    Updates to MP4 CPS 171 10-20-23 10:36:49 PM

--- CPS171_MP4/CPS171_MP4/main.cpp
+++ CPS171_MP4/CPS171_MP4/main.cpp
@@ -1,7 +1,7 @@
 /*
  CPS171_MP4 - Project Name here
  
-
+ 
  
  Created by Oliver McMillen on 10/19/23.
  */
@@ -12,14 +12,10 @@
 
 using namespace std;
 
-int particularAbNumber;
-
-
-
 int main(int argc, const char * argv[]) {
     int numberToInspect = 0;
     int properDivisor = 0;
-
+    
     
     //Iterate through 1-100
     for (int i=0; i <= 1000; i++){
@@ -32,41 +28,43 @@ int main(int argc, const char * argv[]) {
             
             string stringOfProperDivisors = "";
             
-
-            
             //As long as j is less than the number to inspect, continue
             for (int j=1; j < numberToInspect; j++){
                 
                 //If number to inspect divided by j has remainder of 0, print the out to the line.
                 if (numberToInspect % j == 0){
                     properDivisor = j;
-
                     
                     stringOfProperDivisors += to_string(properDivisor) + ", ";
+                    
                     sumOfDivisors += properDivisor;
                     countOfDivisors++;
                 }
                 
             }
-            stringOfProperDivisors.pop_back();
-            stringOfProperDivisors.pop_back();
-            stringOfProperDivisors += ".";
             
-            if (sumOfDivisors > numberToInspect){
-                if (countOfDivisors == 5){
-                    cout <<"this is: " << numberToInspect << endl;
-                    cout << properDivisor << " ";
-                    cout << endl << "The list of proper divisors is: " << stringOfProperDivisors << endl;
-                    cout << "The sum of proper divisors is: " << sumOfDivisors << endl;
-                    cout << "The count of proper divisors is: " << countOfDivisors << endl;
-                    cout << endl;
-                }
+            
+            //Abundannt numbers will be printed to console, if a number has 6 divisors the loop will stop.
+            if (countOfDivisors == 6){
+                cout << numberToInspect <<" is the first natural number with 6 proper divisors." << endl;
+                cout << "The proper divisors of "<< numberToInspect << " are " << stringOfProperDivisors << endl;
+                cout << "The sum of the proper divisors is " << sumOfDivisors
+                << " which is less than the number " << numberToInspect << endl;
+                cout << "There are " << countOfDivisors << " proper divisors for the number " << numberToInspect << endl;
+                cout << endl;
+                break;
+                
+            } else if (sumOfDivisors > numberToInspect){
+                cout <<"The number " << numberToInspect << " is an ABUNDANT number." << endl;
+                cout << "The proper divisors of "<< numberToInspect << " are " << stringOfProperDivisors << endl;
+                cout << "The sum of the proper divisors is " << sumOfDivisors
+                << " which is greater than the number " << numberToInspect << endl;
+                cout << "There are " << countOfDivisors << " proper divisors for the number " << numberToInspect << endl;
+                cout << endl;
             }
         }
-
         
     }
-
     
     return 0;
 }
