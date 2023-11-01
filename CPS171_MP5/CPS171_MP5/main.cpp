7d34d83a6ba3d5c73e9817f1b7ae6bafff1149b1
Oliverishuman <mcmilleno@yahoo.com>
2023-10-31 23:32:15 -0400

    Initial upload for MP5 Functions 10-31-23 11:32:08 PM

---
+++ CPS171_MP5/CPS171_MP5/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CPS171_MP5
+//
+//  Created by Oliver McMillen on 10/31/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
