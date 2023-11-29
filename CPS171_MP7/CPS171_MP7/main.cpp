cb3ae3ce65bfdcbb1d506e4ea0f862eee3ddbe93
Oliverishuman <mcmilleno@yahoo.com>
2023-11-28 19:28:26 -0500

    Updates, formatting is about complete. Need to add functions to check spelling accuracy of word, and function to increase count of error counts.

--- CPS171_MP7/CPS171_MP7/main.cpp
+++ CPS171_MP7/CPS171_MP7/main.cpp
@@ -15,15 +15,20 @@
 
 using namespace std;
 
-list<string> splitString(string sentenceToBeSplit, list<string>& listOfWords)
+list<string> splitString(string sentenceToBeSplit, list<string>& listOfWords, string& correctSpellingWord)
 {
     string word;
     stringstream splitstream(sentenceToBeSplit);
+//    string correctSpellingWord;
     
     //List is initialized as empty each time the function is called
     listOfWords = {};
     
     while (splitstream >> word) {
+        //Sets the correct spelling of the word if not set already
+        if (correctSpellingWord.empty()){
+            correctSpellingWord = word;
+        }
         listOfWords.insert(listOfWords.end(), word);
     }
     
@@ -37,7 +42,10 @@ int main(int argc, const char * argv[]) {
     ifstream inFile;
     string str;
     list<string> listOfWords = {};
-
+    
+    enum errorStatus{CORRECT, SUBSTITUTION, TRANSPOSITION, DELETION, INSERTION, ERROR};
+    errorStatus errorStatus;
+    string errorString;
     
     inFile.open("mp7spelling.txt");
     
@@ -45,22 +53,27 @@ int main(int argc, const char * argv[]) {
     if (inFile.fail())
         cout << endl << "File not found!" << endl;
     else{
-        
         cout << "Spelling Errors - MP3 by Oliver McMillen" << endl << endl;
 
         //While there is a new line with contents, proceed
         while(getline(inFile, str)){
+            string correctSpellingWord;
+
             /*
             Call splitString function to split each word from sentence into a list.
             Reference parameter for the listOfWords so I can continue to use the data
             For each line in the txt file, a new list is created and the previous list is wiped.
             */
-            splitString(str, listOfWords);
+            splitString(str, listOfWords, correctSpellingWord);
             
-            //Check what list of words for each new list created
-            cout << "**** Printing new list now ****" << endl;
+            //Print list of words for each new list created
+            cout << "**** Starting new line ****" << endl;
+            cout << "The word being checked is " << correctSpellingWord << endl << endl;
             for (auto word : listOfWords){
-                cout << word << "\n";
+                cout << "User word is " << word << "\n";
+                
+                
+                cout  << "The user word " << endl << endl;
             }
             cout << endl;
         }
