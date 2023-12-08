b004743a4ff336f6a7248ec51c4c0ef070498cf3
Oliverishuman <mcmilleno@yahoo.com>
2023-12-08 16:49:03 -0500

    Updates to project, code is iterating through teams, and iterating through players adding each player to the array of their respective team

---
+++ CPS171_MP8/CPS171_MP8/PlayerClass.cpp
@@ -0,0 +1,8 @@
+//
+//  PlayerClass.cpp
+//  CPS171_MP8
+//
+//  Created by Oliver McMillen on 12/8/23.
+//
+
+#include "PlayerClass.hpp"
---
+++ CPS171_MP8/CPS171_MP8/PlayerClass.hpp
@@ -0,0 +1,13 @@
+//
+//  PlayerClass.hpp
+//  CPS171_MP8
+//
+//  Created by Oliver McMillen on 12/8/23.
+//
+
+#ifndef PlayerClass_hpp
+#define PlayerClass_hpp
+
+#include <stdio.h>
+
+#endif /* PlayerClass_hpp */
